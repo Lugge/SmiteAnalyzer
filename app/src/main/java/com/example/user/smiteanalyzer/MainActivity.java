c286965c295faa445bae7282717f68e8e9911bcd
proteozym <proteozym@gmail.com>
2017-08-24 11:02:07 +0200

    Initial commit - Colors - Menu Items

--- app/src/main/java/com/example/user/smiteanalyzer/MainActivity.java
+++ app/src/main/java/com/example/user/smiteanalyzer/MainActivity.java
@@ -80,17 +80,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+       
+        if (id == R.id.nav_gods) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_items) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_builds) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_info) {
 
         }
 
